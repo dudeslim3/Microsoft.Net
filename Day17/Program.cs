7fe6cb093fc3a2ae10a7e63eefac51f6040b9d69
dudeslim3 <61380924+dudeslim3@users.noreply.github.com>
2020-11-09 20:55:19 +0530

    Add files via upload

---
+++ Day17/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADO01_ConsoleApplication
+{
+    public class Productlayer
+    {
+       
+        public void Emp()
+        {
+
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = @"Data Source=(localdb)\Projects;Initial Catalog=mydb;Integrated Security=True";
+                SqlCommand cmd = new SqlCommand("Select * from Emp", con);
+
+                con.Open();
+                Console.WriteLine("connected");
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2} ", rdr["Id"], rdr["Name"], rdr["Salary"]);
+                    }
+                }
+            }
+
+        }
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            Productlayer obj = new Productlayer();
+            obj.Emp();
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ Day17/Thread/Thread.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+
+namespace MyThread
+{
+    class Mymath
+    {
+        public int getFact(int num)
+        {
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact *= i;
+            }
+           return fact;
+        }
+    }
+
+
+    class MyClassThread
+    {
+        static Mymath ob = new Mymath();
+        int num;
+        Thread t = null;
+        int ans;
+
+        public MyClassThread(int num, string name)
+        {
+            this.num = num;
+            t = new Thread(this.fact);
+            t.Name = name;
+            t.Start();
+        }
+
+        public void fact()
+        {
+            lock (ob) this.ans = ob.getFact(this.num);
+            Console.WriteLine("Factorial of {0} is {1}",Thread.CurrentThread.Name,this.ans);
+        }
+     
+    }
+    
+    class program
+    {
+       
+        static void Main(string[] args)
+        {
+            MyClassThread t1 = new MyClassThread(5, "Thread-1");
+            MyClassThread t2 = new MyClassThread(4, "Thraed-2");
+
+            Console.Read();
+        }
+
+    }
+}
